c189c2f9c3d699ccfd8d856115498253bd8552e5
Hans Escher <jpescher1970@gmail.com>
2023-07-11 14:12:45 +0200

    test component

---
+++ projects/test-component/src/lib/test-component.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-TestComponent',
+  template: `
+    <p>
+      test-component works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class TestComponentComponent {
+
+}
